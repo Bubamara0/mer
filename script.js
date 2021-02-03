83dc82ae6b5d616edb603baeb7d67778231aa003
Bubamara0 <72171393+Bubamara0@users.noreply.github.com>
2021-02-03 18:28:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -85,7 +85,7 @@ if (jour == dates[5]){
         document.querySelector('#result').textContent=maree[0];
     }else if(heure == horaires[19] || heure == horaires[21]){
         document.querySelector('#result').textContent=maree[1];
-    }else if((heure > horaires[19] && heure < horaires[20]) || (heure > horaires[21] || heure < horaires[22])){
+    }else if(( heure < horaires[19]) || (heure > horaires[20] && heure < horaires[21])){
         document.querySelector('#result').textContent=maree[2];
     }else{
         document.querySelector('#result').textContent=maree[3];
@@ -98,13 +98,135 @@ if (jour == dates[6]){
         document.querySelector('#result').textContent=maree[0];
     }else if(heure == horaires[23] || heure == horaires[25]){
         document.querySelector('#result').textContent=maree[1];
-    }else if((heure > horaires[23] && heure < horaires[24]) || (heure > horaires[25] || heure < horaires[26])){
+    }else if(( heure < horaires[23]) || (heure > horaires[24] && heure < horaires[25])){
         document.querySelector('#result').textContent=maree[2];
     }else{
         document.querySelector('#result').textContent=maree[3];
     }   
 }
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//-------------------NOVEMBER------------//
+
+
+// let dates = [16.11, "17.11", "18.11", "19.11", "20.11"];
+// let horaires = [2.13, 7.32, 14.36, 19.54, 2.57, 8.15, 15.19, 20.38, 3.37, 8.56, 16.00, 21.20, 4.15, 9.37, 16.39, 22.02, 4.52, 10.19, 17.19, 22.47];
+// let maree = ["Basse mer", "Pleine mer", "Marée montante", "Marée descendante"];
+
+// //  
+
+ 
+//   document.querySelector('.check').addEventListener('click', function(){
+//     let heure = Number(document.querySelector('#heure').value);
+//     let jour = Number(document.querySelector('#jour').value);
+ 
+// // Lundi 16 novembre
+//     if(jour==dates[0]){
+//         if(heure == horaires[0] || heure == horaires[2]){
+//             document.querySelector('.result').textContent=maree[0];
+//         }else if(heure == horaires[1] || heure == horaires[3]){
+//             document.querySelector('.result').textContent=maree[1];
+//         }else if((heure > horaires[0] && heure < horaires[1]) || (heure > horaires[2] && heure < horaires[3])){
+//             document.querySelector('.result').textContent=maree[2];
+//         }else{
+//             document.querySelector('.result').textContent=maree[3];
+//         }   
+//     }
+
+// // Mardi 17 novembre
+
+// if (jour == dates[1]){
+//     if(heure == horaires[4] || heure == horaires[6]){
+//         document.querySelector('.result').textContent=maree[0];
+//     }else if(heure == horaires[5] || heure == horaires[7]){
+//         document.querySelector('.result').textContent=maree[1];
+//     }else if((heure > horaires[4] && heure < horaires[5]) || (heure > horaires[6] && heure < horaires[7])){
+//         document.querySelector('.result').textContent=maree[2];
+//     }else{
+//         document.querySelector('.result').textContent=maree[3];
+//     }   
+// }
+
+
+// // Mercredi 18 novembre 
+// if (jour == dates[2]){
+//     if(heure == horaires[8] || heure == horaires[10]){
+//         document.querySelector('.result').textContent=maree[0];
+//     }else if(heure == horaires[9] || heure == horaires[11]){
+//         document.querySelector('.result').textContent=maree[1];
+//     }else if((heure > horaires[8] && heure < horaires[9]) || (heure > horaires[10] && heure < horaires[11])){
+//         document.querySelector('.result').textContent=maree[2];
+//     }else{
+//         document.querySelector('.result').textContent=maree[3];
+//     }   
+// }
+// // Jeudi 19 novembre 
+
+// if (jour == dates[3]){
+//     if(heure == horaires[12] || heure == horaires[14]){
+//         document.querySelector('.result').textContent=maree[0];
+//     }else if(heure == horaires[13] || heure == horaires[15]){
+//         document.querySelector('.result').textContent=maree[1];
+//     }else if((heure > horaires[12] && heure < horaires[13]) || (heure > horaires[14] && heure < horaires[15])){
+//         document.querySelector('.result').textContent=maree[2];
+//     }else{
+//         document.querySelector('.result').textContent=maree[3];
+//     }   
+// }
+
+// // Vendredi 20 novembre
+
+// if (jour == dates[4]){
+//     if(heure == horaires[16] || heure == horaires[18]){
+//         document.querySelector('.result').textContent=maree[0];
+//     }else if(heure == horaires[17] || heure == horaires[19]){
+//         document.querySelector('.result').textContent=maree[1];
+//     }else if((heure > horaires[16] && heure < horaires[17]) || (heure > horaires[18] && heure < horaires[19])){
+//         document.querySelector('.result').textContent=maree[2];
+//     }else{
+//         document.querySelector('.result').textContent=maree[3];
+//     }   
+// }
+//  });
+
+
+
+
+
 
 
 
